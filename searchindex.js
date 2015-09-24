a65d6ddb53647733d4e62909f1e94165742ff0b8
michaelpacer <michaelpacer@gmail.com>
2015-09-24 01:38:00 -0700

    added direct pre block to index.html

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to atest&#8217;s documentation!"],titleterms:{atest:0,document:0,indic:0,tabl:0,welcom:0}})
+Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to atest&#8217;s documentation blah!"],titleterms:{atest:0,blah:0,document:0,indic:0,tabl:0,welcom:0}})
